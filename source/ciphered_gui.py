52650699f175b7c811ddbad899bcfe5a8f833315
William <lalis.william@gmail.com>
2022-11-02 15:47:14 +0100

    Hidden password in the input

--- source/ciphered_gui.py
+++ source/ciphered_gui.py
@@ -11,10 +11,10 @@ class CipheredGUI(BasicGUI):
     def _create_connection_window(self) -> None:
         with dpg.window(label="Connection", pos=(200, 150), width=400, height=300, show=False, tag="connection_windows"):
             
-            for field in ["host", "port", "name", "password"]:
+            for field in [("host", False), ("port", False), ("name", False), ("password", True)]:
                 with dpg.group(horizontal=True):
-                    dpg.add_text(field)
-                    dpg.add_input_text(default_value=DEFAULT_VALUES[field], tag=f"connection_{field}")
+                    dpg.add_text(field[0])
+                    dpg.add_input_text(default_value=DEFAULT_VALUES[field[0]], tag=f"connection_{field[0]}", password=field[1])
 
             dpg.add_button(label="Connect", callback=self.run_chat)
 
